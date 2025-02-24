166390f19b6212da372201c7da76c77f77b316b3
Davi Santana <davisantana.ti@gmail.com>
2025-02-24 20:51:10 -0300

    add rater limiter

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,16 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 
-// Carregando cert SSL para Https
-const options = {
-    key: fs.readFileSync('server.key'),  // Chave privada
-    cert: fs.readFileSync('server.cert') // Certificado
-};
+const rateLimit = require('express-rate-limit');
 
-const PORT = process.env.PORT || 5000;
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`Servidor rodando com HTTPS na porta ${PORT}`);
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // Máximo de 100 requisições por IP a cada 15 minutos
+    message: { error: 'Muitas requisições. Tente novamente mais tarde.' }
 });
+
+// Aplica o Rate Limiter a todas as rotas
+app.use(limiter);
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
@@ -30,3 +30,14 @@ app.use('/movies', movieRoutes);
 const favoriteRoutes = require('./routes/favorites');
 app.use('/favorites', favoriteRoutes);
 
+
+// Carregando cert SSL para Https
+const options = {
+    key: fs.readFileSync('server.key'),  // Chave privada
+    cert: fs.readFileSync('server.cert') // Certificado
+};
+
+const PORT = process.env.PORT || 5000;
+https.createServer(options, app).listen(PORT, () => {
+    console.log(`Servidor rodando com HTTPS na porta ${PORT}`);
+});
